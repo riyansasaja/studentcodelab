49c263800d741c5acdcaadcd63c4736e5c203f61
riyansasaja <riyanboyuhu@gmail.com>
2020-05-11 16:13:36 +0800

    tambah javascript

--- bootstrap/ngobar2/assets/scriptbaru.js
+++ bootstrap/ngobar2/assets/scriptbaru.js
@@ -1,3 +1,17 @@
-$('.page_scrool').on('click', function (e) {
+$('.pagescroll').on('click', function (j) {
+
+    var link = $(this).attr('href');
+
+    var element = $(link);
+
+
+
+    // $('pagescroll').scrollTop(700);
+
+    $('html, body').animate({
+        scrollTop: element.offset().top - 58
+    }, 900, 'swing');
+
+    j.preventDefault();
 
 });

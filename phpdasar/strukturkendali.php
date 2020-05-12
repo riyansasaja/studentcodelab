0d92573b3fdc3e94ce6206ea27625fdf59d80fa5
riyansasaja <riyanboyuhu@gmail.com>
2020-05-12 21:08:23 +0800

    strukturkendali add for|while

--- phpdasar/strukturkendali.php
+++ phpdasar/strukturkendali.php
@@ -1,9 +1,15 @@
 <?php
 
 // for
-echo "//ini pengulangan for <br>";
 for ($i = 0; $i < 5; $i++) {
-    echo "hallo samua <br>";
+    echo "hallo ini for <br>";
 }
 
+echo "<hr>";
+//while
+$i = 0;
+while ($i < 5) {
+    echo "hallo ini while <br>";
+    $i++;
+}
 echo "<hr>";

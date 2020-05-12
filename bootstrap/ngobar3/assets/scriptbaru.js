b44579b69496f70a452febcc61656b1afb6f952d
riyansasaja <riyanboyuhu@gmail.com>
2020-05-12 14:32:49 +0800

    nobar3 add paralax jumbotron

--- bootstrap/ngobar3/assets/scriptbaru.js
+++ bootstrap/ngobar3/assets/scriptbaru.js
@@ -20,5 +20,15 @@ $('.pagescroll').on('click', function (j) {
 $(window).scroll(function () {
     var wScroll = $(this).scrollTop();
 
+    $('.jumbotron img').css({
+        'transform': 'translate(0px,' + wScroll / 4 + '%)'
+    });
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px,' + wScroll / 2 + '%)'
+    });
+    $('.jumbotron p').css({
+        'transform': 'translate(0px,' + wScroll / 1 + '%)'
+    });
+
 
 });
